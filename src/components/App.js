6fa891f28fe486454b5c46a7b7b3e00c4f8664db
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-21 23:33:43 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [selectedOption, setSelectedOption] = useState("");
 
-export default App
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <p>Selected Option: {selectedOption}</p>
+      <div>
+        <button onClick={() => setSelectedOption("Option 1")}>
+          Select Option 1
+        </button>
+      </div>
+      <div>
+        <button onClick={() => setSelectedOption("Option 2")}>
+          Select Option 2
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
