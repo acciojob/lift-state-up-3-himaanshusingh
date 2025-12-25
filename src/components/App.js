ccf01f5e57b0ceeaceb8d76e5b59d32decdd426f
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-25 16:46:28 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,18 @@
 import React, { useState } from "react";
 import "./../styles/App.css";
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
   const [selectedOption, setSelectedOption] = useState("");
 
   return (
-    <div className="parent">
-      <h1>Parent Component</h1>
+    <>
+      <h1 className="parent">Parent Component</h1>
+      <ChildComponent1 setSelectedOption={setSelectedOption} />
+      <ChildComponent2 setSelectedOption={setSelectedOption} />
       <p>Selected Option: {selectedOption}</p>
-      <div>
-        <button onClick={() => setSelectedOption("Option 1")}>
-          Select Option 1
-        </button>
-      </div>
-      <div>
-        <button onClick={() => setSelectedOption("Option 2")}>
-          Select Option 2
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/ChildComponent1.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const ChildComponent1 = ({ setSelectedOption }) => {
+  return (
+    <div>
+      <h2>Child Component 1</h2>
+      <button onClick={() => setSelectedOption("Option 1")}>Option 1</button>
+    </div>
+  );
+};
+
+export default ChildComponent1;
